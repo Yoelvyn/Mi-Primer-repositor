c32e6d011d5f30fdeb690685e0465e340e06b955
Yoelvyn <115593000+Yoelvyn@users.noreply.github.com>
2022-10-13 15:38:22 -0700

    mi segundo commit

--- Calculadora.js
+++ Calculadora.js
@@ -1,15 +1,14 @@
 class Calculadora { 
-sumar( num1, num2) {
-    return num1 + num2;
-}
-restar( num1, num2) {
-    return num1 - num2;
-}
-multiplicar( num1, num2) {
-    return num1 * num2;
-}
-dividir( num1, num2) {
-    return num1 / num2;
-}
-
+    sumar( num1, num2) {
+        return num1 + num2;
+    }
+    restar( num1, num2) {
+        return num1 - num2;
+    }
+    multiplicar( num1, num2) {
+        return num1 * num2;
+    }
+    dividir( num1, num2) {
+        return num1 / num2;
+    }
 }
